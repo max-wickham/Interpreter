9141d50de8e9aaf591820a75eb0d5431a694ab77
Max Wickham <maxwickhammonro@gmail.com>
2022-01-04 23:05:41 +0000

    allocator errors

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -1,204 +1,105 @@
 #include <cstddef>
 #include <cstdint>
-#include <unistd.h>
-#include <utility>
-#include <iostream>
 
-/* Word size, 4 bytes for esp32 */
-using word_t = intptr_t;
+// Word size in bytes
+const int wordSize = 4;
 
-/* Allocated block of memory */
-struct Block
-{
-    // Object header
-    size_t size;
-    bool used;
-    Block *next;
-    // Data
-    word_t data[1];
+// Struct for storing object header
+struct BlockHeader{
+    // The ID of the last sweep this block was marked in
+    int sweepID;
+    // A pointer to the next block
+    BlockHeader *next;
+    // The number of bytes used by the block data
+    size_t dataSize;
+    // The number of bytes of block data that are pointers
+    size_t pointerDataSize;
 };
 
-/* Heap start */
-static Block *heapStart = nullptr;
+// The start of the heap initialised at the begining
+static BlockHeader *heapStart = nullptr;
+// The current end of the heap
+static BlockHeader *heapTop = heapStart;
+static BlockHeader *maxHeapAddress;
 
-/* Top of heap */
-static auto top = heapStart;
-
-/* Align size by word size */
-static inline size_t align(size_t n)
-{
-    return (n + sizeof(word_t) - 1) & ~(sizeof(word_t) - 1);
+// Align a memory size to use a whole number of bytes
+static inline size_t align(size_t dataSize){
+    // TODO test
+    return (dataSize + wordSize - 1) & ~(wordSize - 1);
 }
 
-/*
- * Returns total allocation size, 
- * word_t data[1] reserves one word automatically so this needs to be removed
-*/
-static inline size_t allocSize(size_t size)
-{
-    return size + sizeof(Block) - sizeof(std::declval<Block>().data);
+// Calculate the size of the block header and block data combined
+static inline size_t sizeForAlloc(size_t dataSize){
+    return dataSize + sizeof(BlockHeader);
 }
 
-/* Request memory from OS */
-static Block *requestFromOS(size_t size)
-{
-    // Current heap break.
-    auto block = (Block *)sbrk(0); // (1)
-
-    // OOM.
-    if (sbrk(allocSize(size)) == (void *)-1)
-    { // (2)
-        return nullptr;
-    }
-
-    return block;
-}
-
-/* 
- * Splits the block into two, 
- * returns the pointer to the sub-block of the requested size. 
-*/
-static Block *split(Block *block, size_t size)
-{
-    size_t newSize = allocSize(size);
-    Block *newBlock = block + newSize;
-    newBlock->used = false;
-    // subtract header size and data size from old size
-    newBlock->size = block->size - size - allocSize(0);
-    block->size = size;
+// Split a block into two smaller blocks,
+// return a pointer to the block of the requested size
+static BlockHeader *split(BlockHeader *block, size_t dataSize){
+    BlockHeader *newBlock = block + sizeForAlloc(dataSize);
+    newBlock->sweepID = block->sweepID;
     newBlock->next = block->next;
     block->next = newBlock;
-    return block;
+    newBlock->dataSize = block->dataSize - sizeForAlloc(dataSize);
 }
 
-/* Check if this block can be split. */
-static const size_t min_size = align(sizeof(Block) - sizeof(std::declval<Block>().data));
-static inline bool canSplit(Block *block, size_t size)
-{
-    return block->size - allocSize(size) >= min_size;
+// Check if a block can be split with a given size
+static inline bool canSplit(BlockHeader *block, size_t dataSize){
+    return block->dataSize > dataSize + sizeof(BlockHeader);
 }
 
-/* Allocates a block from the list, splitting if needed. */
-static Block *listAllocate(Block *block, size_t size)
-{
-    // Split the larger block, reusing the free part.
-    if (canSplit(block, size))
-    {
-        block = split(block, size);
-    }
+// Block to start search on, initiliased in initHeap
+static BlockHeader *searchStart;
 
-    block->used = true;
-    block->size = size;
-
-    return block;
-}
-
-/* Previously found block. Updated in `nextFit` */
-static Block *searchStart = heapStart;
-
-/* Reset the heap to the original position. */
-static void resetHeap()
-{
-    // Already reset.
-    if (heapStart == nullptr)
-    {
-        return;
-    }
-
-    // Roll back to the beginning.
-    brk(heapStart);
-
-    heapStart = nullptr;
-    top = nullptr;
-    searchStart = nullptr;
-}
-
-/*
- * Next-fit algorithm.
- *
- * Returns the next free block which fits the size.
- * Updates the `searchStart` of success.
-*/
-static inline Block *nextFit(size_t size)
-{
-    Block *currentBlock = searchStart;
-    if (!currentBlock)
-    {
+// Check if there is a block that can be used,
+// return it if there is and split the block if possible
+static inline BlockHeader *nextFit(size_t dataSize, int sweepID){
+    // Check atleast one object has been allocated
+    if (heapTop == heapStart){
         return nullptr;
     }
-    while (currentBlock->size < size && !currentBlock->used)
-    {
-        // Check if reached the end of the linked list
-        if (currentBlock->next == nullptr)
-        {
-            currentBlock = heapStart;
-        }
-        // Check if returned to the start of the loop
-        else if (currentBlock->next == searchStart)
-        {
-            return nullptr;
-        }
+    BlockHeader *currentBlock = searchStart;
+    while(currentBlock->dataSize < dataSize 
+        && !(currentBlock->sweepID == sweepID)){
+            if (currentBlock->next == nullptr){
+                currentBlock = heapStart;
+            }
+            else if (currentBlock->next == searchStart)
+            {
+                return nullptr;
+            }
     }
-    return listAllocate(currentBlock, size);
+    searchStart = currentBlock;
+    if (canSplit(currentBlock,dataSize)){
+        return split(currentBlock,dataSize);
+    }
+    return currentBlock;
 }
 
-/*
- * Tries to find a block that fits.
-*/
-static Block *findBlock(size_t size)
-{
-    return nextFit(size);
+BlockHeader *allocBlock(size_t dataSize, size_t pointerdataSize, 
+    int sweepID){
+    dataSize = align(dataSize);
+    BlockHeader *block = nextFit(dataSize, sweepID);
+    if (block){
+        block->pointerDataSize = pointerdataSize;
+        return block;
+    }
+    if (heapTop == maxHeapAddress){
+        return nullptr;
+    }
+    BlockHeader *newBlock = heapTop;
+    newBlock->sweepID = sweepID;
+    newBlock->dataSize = dataSize;
+    newBlock->pointerDataSize = pointerdataSize;
+    newBlock->next = newBlock + dataSize + sizeof(BlockHeader); 
+    heapTop = newBlock->next;
+    return newBlock;
 }
 
-/*Returns the object header */
-static Block *getHeader(word_t *data)
-{
-    return (Block *)((char *)data + sizeof(std::declval<Block>().data) -
-                     sizeof(Block));
-}
-
-/* Allocates a block of memory of (at least) `size` bytes */
-word_t *allocBlock(size_t size)
-{
-    size = align(size);
-
-    // ---------------------------------------------------------
-    // 1. Search for an available free block:
-
-    if (auto block = findBlock(size))
-    {
-        return block->data;
-    }
-
-    // ---------------------------------------------------------
-    // 2. If block not found in the free list, request from OS:
-
-    auto block = requestFromOS(size);
-
-    block->size = size;
-    block->used = true;
-
-    // Init heap.
-    if (heapStart == nullptr)
-    {
-        heapStart = block;
-    }
-
-    // Chain the blocks.
-    if (top != nullptr)
-    {
-        top->next = block;
-    }
-
-    top = block;
-
-    // User payload:
-    return block->data;
-}
-
-/* Frees a previously allocated block */
-void freeBlock(word_t *data)
-{
-    auto block = getHeader(data);
-    block->used = false;
+// Initilise the heap
+void initHeap(BlockHeader* start, BlockHeader* end){
+    heapStart = start;
+    heapTop = heapStart;
+    searchStart = heapStart;
+    maxHeapAddress = end;
 }
